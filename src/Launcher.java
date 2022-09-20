9703a8f1c8e8fccd85cfe6a80d17338ff6b53943
Augustin Ponsin <augustin.ponsin@epita.fr>
2022-09-20 16:06:34 +0200

    Partie 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,12 +1,29 @@
 import java.util.Scanner;
 
 public class Launcher {
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Bienvenue !");
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         while (!str.equals("quit")){
-            System.out.println("Unknown command");
+            if (str.equals("fibo")){
+                Scanner number = new Scanner(System.in);
+                System.out.println(fibonacci(Integer.parseInt(number.nextLine())));
+            }
+            if (!str.equals("fibo") && !str.equals("quit")) {
+                System.out.println("Unknown command");
+            }
             scanner = new Scanner(System.in);
             str = scanner.nextLine();
         }
